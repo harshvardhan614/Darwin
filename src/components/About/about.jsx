4e8fc0e289c9b58a0027cae1330136afe12a5d45
Tirth <tirthsolanki29923@gmail.com>
2023-07-21 10:58:07 +0530

    as

--- src/components/About/about.jsx
+++ src/components/About/about.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../../index.css";
 import { darwinPage, logo,  } from "../../assets";
-
+import Aos from "aos";
+import 'aos/dist/aos.css';
 export default function about() {
+  useEffect(()=>{
+    Aos.init({duration:1500})
+  },[])
   return (
-    <div className="flex items-center justify-center w-full max-w-7xl  my-[200px] z-20">
-      <img src={darwinPage} alt="no img" className="w-[580px] w-auto object-contain pb-[20px] "/>
+    <div className="flex items-center justify-center w-full max-w-7xl  my-[200px] z-20" data-aos="fade-right">
+      <img src={darwinPage} alt="no img" className="w-[580px] h-auto object-contain pb-[20px] "/>
       <div className="flex flex-col items-start gap-[20px]">
       <img src={logo} alt="logo" className="h-[50px] w-auto object-contain mb-[20px] "/>
       <p className="ml-[40px]">我々は、ウェブ制作、AIソリューション、そしてweb3テクノロジー <br />
