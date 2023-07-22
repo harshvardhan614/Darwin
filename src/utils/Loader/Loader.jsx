0bd55d16fcbff2c8a9474f77b4d312062a507ea0
nikhilmittalx <nikhilmittal421@gmail.com>
2023-07-22 13:53:07 +0530

    loader

--- src/utils/Loader/Loader.jsx
+++ src/utils/Loader/Loader.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import "./Loader.css"
+import logo from "../../assets/darwinLOGOSVG.svg"
+import logo1 from "../../assets/logo.png"
 
 const Loader = () => {
   return (
     <div className='loading'>
+            <img src={logo1} style={{height: '70px', width: '170px'}} className='imgg absolute top-[30%]'/>
         <div></div>
     </div>
   )
